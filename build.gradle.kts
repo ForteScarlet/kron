6b87136610b96a0ca92583773636dd9016786604
ForteScarlet <ForteScarlet@163.com>
2021-09-27 17:16:32 +0800

    Add gradle.properties to .ignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,24 @@
 @file:Suppress("LocalVariableName")
 
+import org.jetbrains.dokka.gradle.DokkaTask
+
+
+// val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+// compileKotlin.kotlinOptions.suppressWarnings = true
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        verbose = true
+    }
+}
+
+
+
 plugins {
     kotlin("multiplatform") version "1.5.31"
+    `maven-publish`
+    signing
+    id("org.jetbrains.dokka") version "1.5.30"
+
 }
 
 group = "love.forte"
@@ -9,6 +26,7 @@ version = "0.0.1"
 
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
@@ -33,16 +51,17 @@ kotlin {
         }
     }
 
-    jvmTargetConfigure("jvm-j8", "1.8")
-    jvmTargetConfigure("jvm-j17", "16") // TODO wait for 17
-
-
+    jvmTargetConfigure("jvm", "1.8")
+    jvmTargetConfigure("jvm-j17", "16") // TODO waiting for 17 support
 
 
     js(LEGACY) {
         browser()
 
+
     }
+
+
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
@@ -52,8 +71,8 @@ kotlin {
         else -> throw GradleException("Host OS '$hostOs' is not supported in Kotlin/Native.")
     }
 
-    println("nativeTarget >> ${nativeTarget::class}")
-    
+    println("Host OS >> $hostOs")
+
     sourceSets {
         all {
             languageSettings {
@@ -76,8 +95,8 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-        val `jvm-j8Main` by getting
-        val `jvm-j8Test` by getting
+        val jvmMain by getting
+        val jvmTest by getting
         val `jvm-j17Main` by getting
         val `jvm-j17Test` by getting
         val jsMain by getting
@@ -85,4 +104,58 @@ kotlin {
         val nativeMain by getting
         val nativeTest by getting
     }
+
+
+    // Publish
+    // See https://zhuanlan.zhihu.com/p/164446166
+    publishing {
+
+        repositories {
+            maven {
+                val releasesRepoUrl = layout.buildDirectory.dir("repos/releases")
+                val snapshotsRepoUrl = layout.buildDirectory.dir("repos/snapshots")
+
+                println("releasesRepoUrl : $releasesRepoUrl")
+                println("snapshotsRepoUrl : $snapshotsRepoUrl")
+
+                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+
+            }
+        }
+
+
+        publications.configureEach {
+            println("publication-$name")
+        }
+
+    }
+
+    signing {
+        sign(publishing.publications)
+    }
+
 }
+
+// Dokka tasks
+tasks.withType<DokkaTask>().configureEach {
+    // failOnWarning.set(false)
+    // offlineMode.set(true)
+    dokkaSourceSets {
+
+        /*
+        Create custom source set (not known to the Kotlin Gradle Plugin)
+         */
+        register("customSourceSetForJvm") {
+            this.jdkVersion.set(8)
+            this.displayName.set("customForJvm")
+            this.sourceRoots.from(file("src/jvmMain/kotlin"))
+        }
+        register("customSourceSetForJvm-17") {
+            this.jdkVersion.set(17)
+            this.displayName.set("customForJvm17")
+            this.sourceRoots.from(file("src/jvm-j17Main/kotlin"))
+        }
+    }
+}
+
+
